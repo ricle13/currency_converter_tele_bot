4fa1d0a501038b4375bbc33d7a11cbe480796d76
ricle13 <soulderect@gmail.com>
2022-11-11 11:07:51 +0300

    Убрал из гитигнор конфиг.

---
+++ config.py
@@ -0,0 +1,18 @@
+TELE_TOKEN = ''
+API_TOKEN = ''
+GREATINGS = '''
+<b>Добро пожаловать в конвертер валют!</b>
+'''
+HELP = '''
+<em>Пример использования:</em>
+<b>EUR</b> <b>rub</b> <b>10</b>
+
+<b>/help</b> - <em>напомнить способ применения бота</em>
+<b>/values</b> - <em>возвращает список доступных валют</em>
+'''
+VALUES = '''
+RUB - <em>Рубль</em>
+EUR - <em>Евро</em>
+USD - <em>Доллар</em>
+'''
+currencies = ('EUR', 'USD', 'RUB')
